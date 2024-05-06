25257ed18c6b2aa137fb969f71064c2cd6aa1625
Kariuki11 <irungukenneth433@gmail.com>
2024-05-07 00:37:44 +0300

    The new changes

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,65 +1,37 @@
 #!/usr/bin/python3
-"""A module for web application deployment with Fabric."""
+
 import os
-from datetime import datetime
-from fabric.api import env, local, put, run, runs_once, sudo
+from fabric.api import env, put, run
 
+env.hosts = ['3.85.33.120', '54.237.58.217']
 
-# Load server IPs from environment variables (assuming they are set)
-env.user = "username"  # Assuming you have SSH access with username
-env.hosts = [os.getenv("HOST1_IP", ""), os.getenv("HOST2_IP", "")]
-
-@runs_once
-def do_pack():
-    """Archives the static files."""
-    if not os.path.isdir("versions"):
-        os.mkdir("versions")
-    cur_time = datetime.now()
-    output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
-        cur_time.year,
-        cur_time.month,
-        cur_time.day,
-        cur_time.hour,
-        cur_time.minute,
-        cur_time.second
-    )
-    try:
-        print("Packing web_static to {}".format(output))
-        local("tar -cvzf {} web_static".format(output))
-        archive_size = os.stat(output).st_size
-        print("web_static packed: {} -> {} Bytes".format(output, archive_size))
-    except Exception:
-        output = None
-    return output
 
 def do_deploy(archive_path):
-    """Deployment of - static files to the host servers.
-
+    """
+    Deploys the static files to the host servers.
     Args:
-        archive_path (str): The path to the archived static files.
-
+        archive_path (str): The path of the archive to distribute.
     Returns:
-        bool: True on success, False on failure.
+        True if all operations have been done correctly,
+        otherwise returns False.
     """
     if not os.path.exists(archive_path):
         return False
-
     file_name = os.path.basename(archive_path)
     folder_name = file_name.replace(".tgz", "")
     folder_path = "/data/web_static/releases/{}/".format(folder_name)
-
+    success = False
     try:
         put(archive_path, "/tmp/{}".format(file_name))
-        with sudo():
-            run("mkdir -p {}".format(folder_path))
-            run("tar -xzf /tmp/{} -C {}".format(file_name, folder_path))
-            run("rm -rf /tmp/{}".format(file_name))
-            run("mv {}web_static/* {}".format(folder_path, folder_path))
-            run("rm -rf {}web_static".format(folder_path))
-            run("rm -rf /data/web_static/current")
-            run("ln -s {} /data/web_static/current".format(folder_path))
+        run("mkdir -p {}".format(folder_path))
+        run("tar -xzf /tmp/{} -C {}".format(file_name, folder_path))
+        run("rm -rf /tmp/{}".format(file_name))
+        run("mv {}web_static/* {}".format(folder_path, folder_path))
+        run("rm -rf {}web_static".format(folder_path))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(folder_path))
         print('New version deployed!')
-        return True
-    except Exception as e:
-        print(f"Error during deployment: {e}")
-        return False
+        success = True
+    except Exception:
+        success = False
+    return success
